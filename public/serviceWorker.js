2939b41efb11e7bcbb427150e53904fa66e3a7a4
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-08-29 19:06:29 -0400

    add manifest & SW add PWA assets

---
+++ public/serviceWorker.js
@@ -0,0 +1,42 @@
+// This code executes in its own worker or thread
+self.addEventListener("install", event => {
+   if(debug){
+      console.log("SW installed")
+   }
+   self.skipWaiting()
+});
+self.addEventListener("activate", event => {
+   if(debug){
+      console.log("And activated!")
+   }
+   event.waitUntil(clients.claim())
+});
+
+// adding resources from PokeAPI to the cache
+self.addEventListener('fetch', event => {
+
+   // Caching the app shell files and skipping the chrome extensions and firebase functions
+   if(!(event.request.url).startsWith('chrome-extension') && !(event.request.url).startsWith('https://firestore.googleapis') && !(event.request.url).startsWith('https://generativelanguage.googleapis.com')){
+      event.respondWith(
+         caches.match(event.request.url).then(response => {
+            if(response != undefined){ // match always resolves, but only if succeed it will have a value
+               return response
+            }
+            else{
+               return fetch(event.request).then(response => {
+                  let clone = response.clone() // cloning the response because I will put one copy on cache and return the other one
+
+                  caches.open('app-shell').then(cache => {
+                     cache.put(event.request.url, clone) // storing the clone of the response with the request as a key
+                  })
+                  return response
+               })
+            }
+         })
+         .catch(e => {
+            console.error(e)
+         })
+      )
+   }
+
+ })
