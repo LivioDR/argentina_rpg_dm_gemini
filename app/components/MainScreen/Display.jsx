1caea9d0b1b1eaced1693637b1ef7fe4691ca589
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-08-30 12:31:07 -0400

    added reactSpinner changed layout from vh to %

--- app/components/MainScreen/Display.jsx
+++ app/components/MainScreen/Display.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { PacmanLoader } from "react-spinners";
 
 const Display = ({loading, modelResponse}) => {
     return(
@@ -6,23 +7,26 @@ const Display = ({loading, modelResponse}) => {
         style={{
             width: '80%', 
             maxWidth: '500px',
-            margin: '5% auto', 
+            margin: '0% auto', 
             wordWrap: 'break-word', 
             overflowY: 'scroll', 
-            height: '60vh',
+            height: '60%',
             backgroundColor: 'lightgray',
             color: 'black',
             borderRadius: '50px',
             }}>
-            <pre style={{whiteSpace: 'pre-wrap', padding: '10%'}}>
+            <pre style={{whiteSpace: 'pre-wrap', padding: '10%', height: '100%',}}>
             {!loading && modelResponse}
             {loading &&
                 <div style={{
+                display: 'grid',
+                placeContent: 'center',
                 margin: 'auto auto',
-                height: '100vh',
-                width: '100vw',
+                height: '100%',
+                width: '100%',
                 }}>
-                Loading...
+                <p style={{margin: '1%', width: '100%', textAlign: 'center',}}>Loading</p>
+                <PacmanLoader />
                 </div>
             }
             </pre>
