a02654af894bc4dc04494823df296496dad53922
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-09-02 15:11:55 -0400

    added metadata to layout

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Press_Start_2P } from "next/font/google";
+import Head from "next/head";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
@@ -12,6 +13,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>TermoArgenta RPG</title>
+        <meta name="title" content="TermoArgenta RPG" />
+        <meta name="description" content="TermoArgenta RPG is an AI-powered game where you can interact in real-time with a customized Dungeon Master that will guide you through your campaigns." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://termoargentarpg.vercel.app/background.jpeg" />
+        <meta property="og:title" content="TermoArgenta RPG" />
+        <meta property="og:description" content="TermoArgenta RPG is an AI-powered game where you can interact in real-time with a customized Dungeon Master that will guide you through your campaigns." />
+        <meta property="og:image" content="https://termoargentarpg.vercel.app/background.jpeg" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://termoargentarpg.vercel.app/" />
+        <meta property="twitter:title" content="TermoArgenta RPG" />
+        <meta property="twitter:description" content="TermoArgenta RPG is an AI-powered game where you can interact in real-time with a customized Dungeon Master that will guide you through your campaigns." />
+        <meta property="twitter:image" content="https://termoargentarpg.vercel.app/background.jpeg" />
+      </Head>
       <SpeedInsights/>
       <body className={pressStart.className}>{children}</body>
     </html>
