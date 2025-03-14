5485f8507f29006f59a904416507f23eb0768c06
LivioDR <liviodavidreinoso@yahoo.com.ar>
2025-03-14 11:32:12 -0400

    feat: update generative model to gemini-2.0-flash

--- app/api/gemini/route.js
+++ app/api/gemini/route.js
@@ -8,7 +8,7 @@ const API_KEY = process.env.API_KEY
 // Access your API key (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(API_KEY);
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash"});
 model.safetySettings = [
   {
     category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
